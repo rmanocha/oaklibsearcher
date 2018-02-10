ca01688f0d9dfb50cf50d7f5aad8a082edaabafb
Rishabh Manocha <rmanoch@upwork.com>
2018-02-10 11:28:22 -0800

    Actually sending an email now.

--- oaklibnotifier.py
+++ oaklibnotifier.py
@@ -1,8 +1,13 @@
 from oaklibapi import OaklandLibraryAPI
 from goodreads_api import GoodreadsQueryAPI
 
+import smtplib
+
 from settings import GOODREADS_ACCESS_KEY, GOODREADS_USER_ID, \
-        NOTIFICATIONS_COUNT
+        NOTIFICATIONS_COUNT, NOTIFICATION_RECIPIENT, \
+        NOTIFICATION_SENDER_USER, NOTIFICATION_SENDER_PASS
+
+NOTIFICATION_SUBJECT = "Books available at Oakland Library"
 
 gdr = GoodreadsQueryAPI(GOODREADS_USER_ID, GOODREADS_ACCESS_KEY)
 
@@ -21,5 +26,24 @@ for book in gdr.get_books():
     if len(books_available) >= NOTIFICATIONS_COUNT:
         break
 
+notification_message = ""
+
 for olib in books_available:
-    print("Book with title={} is available".format(olib.title()))
+    msg = "Book with title={} is available".format(olib.title())
+    print(msg)
+    notification_message += msg + "\n"
+
+if notification_message:
+    email_msg = "Subject: {}\n\n{}".format(NOTIFICATION_SUBJECT, notification_message)
+
+    print("Connecting to gmail")
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(NOTIFICATION_SENDER_USER, NOTIFICATION_SENDER_PASS)
+    print("Connected to gmail")
+
+    print("Sending email")
+    server.sendmail(NOTIFICATION_SENDER_USER, NOTIFICATION_RECIPIENT, 
+            email_msg)
+    server.quit()
+    print("Email sent")
