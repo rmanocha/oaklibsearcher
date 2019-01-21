76375c55b4b3173ed93978a92f73ff279f9fca16
Rishabh Manocha <rmanoch@upwork.com>
2019-01-21 10:29:52 -0800

    We can now generate an RSS response

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import aiohttp
 import asyncio
-from quart import Quart, jsonify, request
+from quart import Quart, jsonify, request, Response
 
 from oaklibapi import OaklandLibraryAPI, BranchesNotKnownException, BookNotFoundException
 from goodreads_api import GoodreadsQueryAPI
@@ -40,26 +40,32 @@ async def get_books_branches():
         for book in gdr.get_books():
             values.append(asyncio.create_task(print_book_data(book, session)))
 
-            for value in values:
-                books_available.append(await value)
+        for value in values:
+            books_available.append(await value)
 
     return books_available
 
 @app.route("/oaklibatom/available_books")
 async def get_available_books():
-    return jsonify(await get_books_branches())
+    books = await get_books_branches()
+    ret_books = []
+    for book in books:
+        if book:
+            ret_books.append({"title": book.title, "isbn": book.isbn, "branches": book.branches})
+    return jsonify(ret_books)
 
 @app.route("/oaklibatom/available_books.atom")
-def get_available_books_rss():
-    books_available = get_books_branches()
+async def get_available_books_rss():
+    books_available = await get_books_branches()
 
-    feed = AtomFeed("Available Books", feed_url=request.url_root + "oaklibatom/available_books.atom",
+    feed = AtomFeed("Available Books",
+            feed_url=request.url_root + "oaklibatom/available_books.atom",
             url=request.url_root)
     for book in books_available:
-        if book["available"]:
-            content = "{} (isbn={}) is available at {}".format(book["title"],
-                    book["isbn"], ",".join(book["branches"]))
-            feed.add(book["title"], content, updated=datetime.datetime.now(),
-                    url=request.url_root + book["isbn"], content_type="text")
+        if book and book.branches:
+            content = "{} (isbn={}) is available at {}".format(book.title,
+                    book.isbn, ",".join(book.branches))
+            feed.add(book.title, content, updated=datetime.datetime.now(),
+                    url=request.url_root + book.isbn, content_type="text")
 
-    return feed.get_response()
+    return Response(response=feed.to_string(), mimetype="application/atom+xml")
